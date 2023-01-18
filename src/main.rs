c6424f87e5135e9ea0198eb038135a309cf7702a
Dylan Turner <dylantdmt@gmail.com>
2023-01-18 12:59:06 -0600

    main: added cli args support
    I added the args module to the file, and now I'm using it.
    
    A simple match statement with some TODOs for each subcommand now makes
    up the main method. These will be the next source of fleshing out.
    
    Signed-off-by: Dylan Turner <dylantdmt@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,26 @@
  * Description: Entry point for the App Image Package Manager
  */
 
+mod args;
+
+use clap::Parser;
+use crate::args::{
+    Args, Commands
+};
+
 fn main() {
-    println!("Hello, world!");
+    match Args::parse().command {
+        Commands::Install { .. } => {
+            // TODO: Install packages
+        }, Commands::Remove { .. } => {
+            // TODO: Remove packages
+        }, Commands::Upgrade => {
+            // TODO: Upgrade packages
+        }, Commands::List => {
+            // TODO: List installed packages
+        }, Commands::Run { .. } => {
+            // TODO: Launch an installed application
+        }
+    }
 }
+
