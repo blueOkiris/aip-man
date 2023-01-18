75b9b5d0b66d5a152eb49b29302595aed0b3f39b
Dylan Turner <dylantdmt@gmail.com>
2023-01-18 12:56:26 -0600

    args: created to store cli command parsing
    I'm using clap because it is simple and easy to use and makes the chore
    of parsing cli arguments take only a few seconds to maintain and create.
    
    The clap library currently works by defining arguments in a struct using
    derive macros. This struct is stored here, in a separate file.
    
    Signed-off-by: Dylan Turner <dylantdmt@gmail.com>

---
+++ src/args.rs
@@ -0,0 +1,43 @@
+/*
+ * Author(s): Dylan Turner
+ * Description: Parse cli arguments
+ */
+
+use clap::{Parser, Subcommand};
+
+/// Structure defining CLI command and arguments.
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+    /// One of the commands: install <pkg>, remove <pkg>, upgrade, etc.
+    #[command(subcommand)]
+    pub command: Commands
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Commands {
+    /// Installs an AppImage from the global repo.
+    Install {
+        /// Package to install.
+        package: String
+    },
+
+    /// Removes an installed AppImage.
+    Remove {
+        /// Package to uninstall.
+        package: String
+    },
+
+    /// Upgrade installed packages.
+    Upgrade,
+
+    /// List installed packages.
+    List,
+
+    /// Run an installed application.
+    Run {
+        /// Installed application to run.
+        app: String
+    }
+}
+
